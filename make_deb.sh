eb110990cf8f2b791d0e72a085d0c7a74aa3ccd7
Alexander Rykovanov <rykovanov.as@gmail.com>
2014-01-09 21:23:28 +0400

    Debian package.

--- make_deb.sh
+++ make_deb.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-tar -xvf opcua-wsdl-0.1.2.tar.gz
-#cp exe/configs/opcuaserver opcuaserver-0.1.2/debian/opcuaserver.init
-cd opcua-wsdl-0.1.2
-dh_make -y -m -c lgpl3 -e rykovanov.as@gmail.com -f ../opcua-wsdl-0.1.2.tar.gz
-dpkg-buildpackage -rfakeroot $*
+tar -xvf opcua-wsdl-${VERSION}.tar.gz
+cd opcua-wsdl-${VERSION}
+dh_make -y -m -c lgpl3 -e rykovanov.as@gmail.com -f ../opcua-wsdl-${VERSION}.tar.gz
+dpkg-buildpackage -rfakeroot -nc -F
 
