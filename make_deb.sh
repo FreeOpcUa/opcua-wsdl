f6e0d0f1cbb9b743a7315d976fed7b3fe0dd11af
Alexander Rykovanov <rykovanov.as@gmail.com>
2014-01-11 19:22:21 +0400

    Additiona dpkg-buildpackage options can be set with environment variable DPKG_BUILDPACKAGE_OPTS

--- make_deb.sh
+++ make_deb.sh
@@ -2,5 +2,5 @@
 tar -xvf opcua-wsdl-${VERSION}.tar.gz
 cd opcua-wsdl-${VERSION}
 dh_make -y -m -c lgpl3 -e rykovanov.as@gmail.com -f ../opcua-wsdl-${VERSION}.tar.gz
-dpkg-buildpackage -rfakeroot -nc -F
+dpkg-buildpackage -rfakeroot -nc -F ${DPKG_BUILDPACKAGE_OPTS}
 
